745402f5ac0c845a6d2655947fe57c1528e96fce
Bill Drayton <billd@drayvest.com>
2022-06-07 13:15:27 +1000

    1. Add RecipeTabView as the starting point for the App.

---
+++ Recipe List/Views/RecipeTabView.swift
@@ -0,0 +1,40 @@
+//
+//  RecipeTabView.swift
+//  Recipe List
+//
+//  Created by Bill Drayton on 6/6/2022.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+        .frame(height: 880)
+//        .tabViewStyle(.page)  //Scrolls left/right rather than icons @ bottom of screen
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
